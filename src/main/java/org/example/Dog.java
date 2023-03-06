2e43d8d732fa8d0258c138d79abc62f2df7a7eae
Jinxuan <you@example.com>
2023-03-06 17:07:29 +1100

    dog

--- src/main/java/org/example/Dog.java
+++ src/main/java/org/example/Dog.java
@@ -5,4 +5,33 @@ public class Dog {
     private String name;
     private int age;
 
+    public Dog(String type, String name, int age) {
+        this.type = type;
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
--- src/test/java/DogTest.java
+++ src/test/java/DogTest.java
@@ -1,2 +1,9 @@
+import org.example.Dog;
+import org.junit.Test;
+
 public class DogTest {
+    @Test
+    public void createTest(){
+        Dog dog = new Dog("big","Wangwang",1);
+    }
 }
