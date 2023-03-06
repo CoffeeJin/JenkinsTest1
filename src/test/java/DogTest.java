9ae4c315eb110b1cafde29bdd4bad02f7c9bc471
Jinxuan <you@example.com>
2023-03-06 17:09:48 +1100

    b

--- src/test/java/DogTest.java
+++ src/test/java/DogTest.java
@@ -6,4 +6,9 @@ public class DogTest {
     public void createTest(){
         Dog dog = new Dog("big","Wangwang",1);
     }
+    @Test
+    public void createTest1(){
+        Dog dog = new Dog("big","Wangwang",1);
+    }
+
 }
